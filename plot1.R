c5d54036222456b2c1c2fd92693fdff35ee3be6a
Mark Anderson <hyPagnIdqp.github.com@ma7.org>
2015-01-24 11:40:15 -0600

    finish plot1 and move on

--- plot1.R
+++ plot1.R
@@ -2,6 +2,9 @@
 #  Mark Anderson
 #  plot1.R
 
+# Q: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+# A: Yes, the graph shows a decline in the total PM2.5 emmissions
+
 url  <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
 zipfile <- 'exdata-data-NEI_data.zip'
 file_scc_rds <- 'Source_Classification_Code.rds'
@@ -21,6 +24,9 @@ Summary    <- readRDS(file_sum_rds)
 
 EmissionsPerYear <- aggregate(Emissions ~ year, Summary, sum)
 
+strn <- "Year\n\n\n\nThis is a silly and overly long\n
+         title that I want to use on my plot"
+
 png(filename='plot1.png',width=480,height=480,units='px')
 plot(
   EmissionsPerYear$year,
@@ -29,6 +35,8 @@ plot(
   xlim=c(1998,2008),
   xlab='Year',
   ylab='PM2.5 Emmisions (tons)',
+  cex.sub = 0.85,
+  sub= 'A decline in Emissions is seen year over year.',
   type='b')
 dev.off()
 
